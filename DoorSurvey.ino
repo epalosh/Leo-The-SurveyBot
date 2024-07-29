9c84f7000141908f7bbed76a823de1c45aa00091
Ethan Palosh <109639295+epalosh@users.noreply.github.com>
2024-07-29 11:25:10 -0700

    Committing code from 02/24
    This code ran on Leo the SurveyBot for about a month while he was operational. Leo was unfortunately damaged by some passerby in the dorm and was unable to be repaired. :/ Rest in peace, SurveyBot.

--- DoorSurvey.ino
+++ DoorSurvey.ino
@@ -1,28 +1,20 @@
-//Door survey bot w/ LCD screen
-//by Ethan Palosh
+// Door survey bot w/ LCD screen
+// by Ethan Palosh
+
+// Questions are asked in a "this or that" format
 
 //To change each day:
-///////////////////////////////
-///////////////////////////////
-///////////////////////////////
-
-//Questions are asked in a "this or that" format
-String choiceOne = "Spotify";
-String choiceTwo = "Apple Music";
+String choiceOne = "Cats";
+String choiceTwo = "Dogs";
 String bottomText = "----------------------------------------";
-//LCD lines are 40 characters long
-///////////////////////////////
-///////////////////////////////
-///////////////////////////////
+// Note: LCD lines are 40 characters long
 
 /* List of functions:
-
 void clearMemorySequence(int &numchoiceOne, const int addressOne, int &numchoiceTwo, const int addressTwo)
 void selectionSequence(const String choice, const int memoryAddress, int &count)
 void displayCalculating(int msDelay)
 void EEPROMWriteInt(int address, int value)
 int EEPROMReadInt(int address)
-
 */
 
 // includes the library code:
@@ -45,7 +37,7 @@ const int ledPin = 10;
 
 void setup() {
 
-  //counters - read the integer from EEPROM
+  // counters - read the integer from EEPROM
   numChoiceOne = EEPROMReadInt(0);
   numChoiceTwo = EEPROMReadInt(10);
 
@@ -61,44 +53,40 @@ void setup() {
   digitalWrite(ledPin, HIGH);
 }
 
-//main loop
+// main loop
 void loop() {
 
-  //LCD setup and delay for scroll
+  // LCD setup and delay for scroll
   lcd.scrollDisplayLeft();
   lcd.setCursor(0, 0);
   delay(200);
   
   // set the cursor to column 0, line 0
   lcd.setCursor(0, 0);
-  //idle message
-  lcd.print(choiceOne + " or " + choiceTwo + "?" + "      Vote!!");
-  //additional info if added
+
+  // idle message
+  lcd.print(choiceOne + " or " + choiceTwo + "?" + "         Vote!!");
+
   lcd.setCursor(0, 1);
   lcd.print(bottomText);
 
-  //checks and stores state of buttons each iteration of the loop
+  // checks and stores state of buttons each iteration of the loop
   int one = digitalRead(choiceOnePin);
   int two = digitalRead(choiceTwoPin);
 
-  //conditionals:
-
-
+  // conditionals:
   if(one == 1){
     delay(200);
     if(two == 1){
       lcd.clear();
       delay(100);
-      lcd.setCursor(0, 0);
-      lcd.print(choiceOne + " " + numChoiceOne);
-      lcd.setCursor(0, 1);
-      lcd.print(choiceTwo + " " + numChoiceTwo);
-      delay(5000);
 
-      //rechecks the buttons' state
+      displayResults(5000);
+
+      // rechecks the buttons' state
       one = digitalRead(choiceOnePin);
       two = digitalRead(choiceTwoPin);
-      //if they are still depressed, clear memory
+      // if they are still depressed, clear memory
       if(two == 1 && one == 1){
         clearMemorySequence(numChoiceOne, 0, numChoiceTwo, 10);
       }
@@ -109,11 +97,8 @@ void loop() {
 
     delay(300);
 
-    lcd.setCursor(0, 0);
-    lcd.print(choiceOne + " " + numChoiceOne);
-    lcd.setCursor(0, 1);
-    lcd.print(choiceTwo + " " + numChoiceTwo);
-    delay(5000);
+    displayResults(500);
+
     lcd.clear();
   }
 
@@ -122,13 +107,12 @@ void loop() {
     if(one == 1){
       lcd.clear();
       delay(100);
-      lcd.setCursor(0, 0);
-      lcd.print(choiceOne + " " + numChoiceOne);
-      lcd.setCursor(0, 1);
-      lcd.print(choiceTwo + " " + numChoiceTwo);
-      delay(5000);
+
+      displayResults(5000);
+
       one = digitalRead(choiceOnePin);
       two = digitalRead(choiceTwoPin);
+      
       if(two == 1 && one == 1){
         clearMemorySequence(numChoiceOne, 0, numChoiceTwo, 10);
       }
@@ -138,24 +122,16 @@ void loop() {
     selectionSequence(choiceTwo, 10, numChoiceTwo);
 
     delay(300);
-    lcd.setCursor(0, 0);
-    lcd.print(choiceOne + " " + numChoiceOne);
-    lcd.setCursor(0, 1);
-    lcd.print(choiceTwo + " " + numChoiceTwo);
-    delay(5000);
+
+    displayResults(500);
+    
     lcd.clear();
   }
 }
 
+// functions!!
 
-
-
-
-
-
-//functions!!
-
-//clears system memory
+// clears system memory
 void clearMemorySequence(int &numchoiceOne, const int addressOne, int &numchoiceTwo, const int addressTwo) {
   lcd.clear();
   lcd.print("Clearing memory");
@@ -167,7 +143,7 @@ void clearMemorySequence(int &numchoiceOne, const int addressOne, int &numchoice
   lcd.clear();
 }
 
-//Registers the selection and shows the user. Then increments the selection count in memory
+// Registers the selection and shows the user. Then increments the selection count in memory
 void selectionSequence(const String choice, const int memoryAddress, int &count) {
   lcd.clear();
   delay(300);
@@ -178,25 +154,20 @@ void selectionSequence(const String choice, const int memoryAddress, int &count)
   delay(1000);
   lcd.clear();
 
-  displayCalculating(700);
-
   EEPROMWriteInt(memoryAddress, ++count);
 
   lcd.clear();
 }
 
-/*
-//displays the current results
-void displayResults(const String choiceOne, const int numChoiceOne, const String choiceTwo, const int numChoiceTwo, int msTime) {
+void displayResults(int msDelay){
   lcd.setCursor(0, 0);
-  lcd.print(choiceOne + " " + numChoiceOne);
-  lcd.setCursor(0, 1);
-  lcd.print(choiceTwo + " " + numChoiceTwo);
-  delay(msTime);
+    lcd.print(choiceOne + " " + numChoiceOne);
+    lcd.setCursor(0, 1);
+    lcd.print(choiceTwo + " " + numChoiceTwo);
+    delay(msDelay);
 }
-*/
 
-//Displays "Calculating..." on the screen for a determined amount of time
+// Displays "Calculating..." on the screen for a determined amount of time
 void displayCalculating(int msDelay) {
   lcd.setCursor(0, 0);
   lcd.print("Calculating...");
